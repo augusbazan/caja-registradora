ab03d24403ce7fcc3f0b380d365d71803dd75fff
augusbazan <bazanaugusto06@gmail.com>
2026-08-10 09:48:45 -0300

    carga de un producto

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,4 +1,13 @@
 Console.WriteLine("==== KIOSCO EL BETO ====");
 Console.Write("Nombre del cajero: ");
-string name =  Console.ReadLine();
-Console.WriteLine($"Bienvenido {name}, Caja abierta");
+string cashName =  Console.ReadLine();
+Console.WriteLine($"Bienvenido {cashName}, Caja abierta");
+
+Console.WriteLine("Agregue un Producto");
+Console.Write("Nombre del producto: ");
+string productName = Console.ReadLine();
+Console.Write("Precio del producto: ");
+decimal productPrice = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine($"=== Producto creado exitosamente ===");
+Console.WriteLine($"Producto: {productName}, Precio: {productPrice} ");
